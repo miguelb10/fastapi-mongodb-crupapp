4312c20daa2ac0d72fdc0c6b005cf471fedcf98a
miguelb10 <miguel.blas_03@hotmail.com>
2021-09-29 00:32:38 -0500

    add cors

--- index.py
+++ index.py
@@ -1,9 +1,21 @@
 #Import statements
 from fastapi import FastAPI
 from routes.student import student_router
+from fastapi.middleware.cors import CORSMiddleware
+
+client_apps = ['http://localhost:3000']
 
 #Create app
 app = FastAPI()
 
 #Register your router
 app.include_router(student_router)
+
+#Register App with CORS middleware to allow resources sharing between differents domains/origin
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=client_apps,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
--- routes/student.py
+++ routes/student.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from models.student import Student
 from config.database import connection
 from schemas.student import studentEntity, listOfStudentEntity
+from bson import ObjectId
 
 student_router = APIRouter()
 
@@ -15,8 +16,29 @@ async def hello_world():
 async def find_all_students():
     return listOfStudentEntity(connection.local.student.find())
 
+#get one student with matching id
+@student_router.get('/students/{studentId}')
+async def find_student_by_id(studentId):
+    return studentEntity(connection.local.student.find_one({"_id": ObjectId(studentId)}))
+
 #creating a student
 @student_router.post('/students')
 async def create_student(student: Student):
     connection.local.student.insert_one(dict(student))
     return listOfStudentEntity(connection.local.student.find())
+
+#update a student
+@student_router.put('/student/{studentId}')
+async def update_student(studentId, student: Student):
+    #find the student and then update it with new student data
+    connection.local.student.find_one_and_update(
+        {"_id": ObjectId(studentId)},
+        {"$set": dict(student)}
+    )
+    return studentEntity(connection.local.student.find_one({"_id": ObjectId(studentId)}))
+
+#delete a student
+@student_router.delete('/student/{studentId}')
+async def delete_student(studentId):
+    #find the student deletes it and also returns the same student object
+    return studentEntity(connection.local.student.find_one_and_delete({"_id": ObjectId(studentId)}))
